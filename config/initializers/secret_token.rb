b674c396038bddb8a6203159a17ce4572d3c4362
Timothy Macdonald <tmacdonald@groupon.com>
2014-05-01 19:50:24 -0500

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fortuneate::Application.config.secret_key_base = '47f2c029b632eee52071f04bda7e1d705e93f883a27efaa3407cb15778250fa1e4c2dcc2ba6c083501931d733b9a289ce9cd0bcb84aeaa72701b3ed7253dafc4'
